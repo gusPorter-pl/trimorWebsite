f9595f0aac885fbdc92079730ba12531fc5234ab
gusPorter-pl <apor417@aucklanduni.ac.nz>
2020-08-15 18:48:53 +1200

    Added json storage

---
+++ scripts/html_to_json.py
@@ -0,0 +1,20 @@
+def get_html():
+    filename = ""
+    pass
+
+def html_text_to_text(input_text):
+    pass
+
+def text_to_json(text):
+    pass
+
+def write_json(json_text):
+    pass
+
+def main():
+    html_text = get_html()
+    text = html_text_to_text(html_text)
+    json_text = text_to_json(text)
+    write_json(json_text)
+
+main()
