b49f76222f997ca70ed31d351283e3e89b4e1d9b
Gus Porter <kiwiangus360@gmail.com>
2020-10-23 18:05:42 +1300

    Added create article file

---
+++ scripts/create_article.py
@@ -0,0 +1,22 @@
+import json
+import sys
+"""
+Creates an empty html file
+"""
+
+def get_article_type():
+    pass
+
+def get_article():
+    pass
+
+def create_file(article_type, article):
+    pass
+
+def main():
+    article_type = get_article_type()
+    article = get_article()
+    create_file(article_type, article)
+
+if __name__ == "__main__":
+    main()
